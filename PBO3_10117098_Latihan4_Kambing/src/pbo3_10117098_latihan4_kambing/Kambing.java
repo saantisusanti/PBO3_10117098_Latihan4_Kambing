1d6e55ed765a581b9eb51ac9460577b69cb2628a
Santi Susanti <43363256+saantisusanti@users.noreply.github.com>
2018-09-23 00:10:49 +0700

    Add files via upload

---
+++ PBO3_10117098_Latihan4_Kambing/src/pbo3_10117098_latihan4_kambing/Kambing.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117098_latihan4_kambing;
+
+/**
+ *
+ * @author 
+ * NAMA  : Santi Susanti
+ * KELAS : PBO3
+ * NIM   : 10117098
+ */
+public class Kambing {
+  public void tambahKambing(){  
+    //deklarasi variabel lokal
+    int jumlahKambing = 0;
+    
+    jumlahKambing = jumlahKambing + 5;
+    System.out.println("Jumlah Kambing setelah ditambah: " +jumlahKambing);
+    }
+}
---
+++ PBO3_10117098_Latihan4_Kambing/src/pbo3_10117098_latihan4_kambing/PBO3_10117098_Latihan4_Kambing.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117098_latihan4_kambing;
+
+/**
+ *
+ * @author 
+ * NAMA  : Santi Susanti
+ * KELAS : PBO3
+ * NIM   : 10117098
+ */
+public class PBO3_10117098_Latihan4_Kambing {
+
+    
+    public static void main(String[] args) {
+        Kambing kambingJantan = new Kambing();
+        kambingJantan.tambahKambing();
+    }
+    
+}
